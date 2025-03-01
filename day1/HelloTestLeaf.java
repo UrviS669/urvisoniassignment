15709925adb385388ca7549b701905abc2132b79
UrviS669 <urvi_soni@live.com>
2025-03-01 18:29:16 +0000

    Add files via upload
    Day 1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+//type main and do ctrl+space
+public static void main(String[] args) {
+	
+	//type syso and do ctrl+space
+	System.out.println("happy learning");
+	
+	System.out.println("Welcome to TestLeaf");
+	
+	
+}
+}
---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class LearnDataTypes {
+	//type main and do ctrl+space
+	public static void main(String[] args) {
+		
+		int regNo=456556;
+		
+		boolean isACar=true;
+		
+		float speed=0.05f;
+		
+		char logo='S';
+		
+		String name="Skoda";
+		
+		long phNo= 1234567890;
+		
+		double capacity=124.56545656;
+		
+		System.out.println("THE LOGO OF THE CAR IS:"+logo);
+
+		
+		
+	}
+
+}
